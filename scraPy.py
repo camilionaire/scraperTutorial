6467f0d6861a0945e7a6778b248356c32ee4aa85
Camilo <camilo3@pdx.edu>
2021-08-09 16:28:18 -0700

    messy working scraper

--- scraPy.py
+++ scraPy.py
@@ -1,8 +1,34 @@
 import requests
 from bs4 import BeautifulSoup
 
-def main():
-	print("Hello World")
+# url of what you want to get
+# header you can look up online, i don't think it's necessarily necessary.
+url = "https://www.pdx.edu/registration/academic-calendar"
+headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:90.0) Gecko/20100101 Firefox/90.0"}
 
-if __name__ == "__main__":
-	main()
+# gets the url through with the header...
+r = requests.get(url, headers=headers)
+# I don't really know what this does
+soup = BeautifulSoup(r.content, "html.parser")
+
+# finds all the spans with the particular class.
+events = soup.find_all('span', class_ = 'title')
+# dates = soup.find_all('var', class_ = "atc_date_start")
+names = []
+for item in events:
+	names.append(item.text.replace("  ", "").replace("\n", ""))
+	# names.append(item.text.replace("\n          ", "").replace("\n        ", "").replace("\n", ""))
+# 	names.append(item.find("span", class_ = "title").text)
+
+for i in range(0, 10):
+	print(names[i])
+
+# print(events[:10])
+# print(names[:10])
+# print(dates[:10])
+
+# def main():
+# 	print("Hello World")
+
+# if __name__ == "__main__":
+# 	main()
