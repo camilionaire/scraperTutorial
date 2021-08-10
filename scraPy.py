ecf61180ef536cf31b3045cdcb45e3b6b738160c
Camilo <camilo3@pdx.edu>
2021-08-09 17:22:16 -0700

    'final' product, loads to a csv using pandas

--- scraPy.py
+++ scraPy.py
@@ -1,37 +1,43 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
 
-# url of what you want to get
-# header you can look up online, i don't think it's necessarily necessary.
-url = "https://www.pdx.edu/registration/academic-calendar"
-headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:90.0) Gecko/20100101 Firefox/90.0"}
+main_list = []
 
-# gets the url through with the header...
-r = requests.get(url, headers=headers)
-# I don't really know what this does
-soup = BeautifulSoup(r.content, "html.parser")
+def extract(url):
+	# url of what you want to get
+	# header you can look up online, i don't think it's necessarily necessary.
+	headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:90.0) Gecko/20100101 Firefox/90.0"}
 
-# finds all the spans with the particular class.
-events = soup.find_all('span', class_ = 'title')
-mesDates = soup.find_all('var', class_ = "atc_date_start")
-names, dates = [], []
+	# gets the url through with the header...
+	r = requests.get(url, headers=headers)
+	# I don't really know what this does
+	soup = BeautifulSoup(r.content, "html.parser")
 
-for item in events:
-	# .strip() replaces all of the extra whitespace n the line.
-	names.append(item.text.strip().replace("\n", ""))
-for date in mesDates:
-	dates.append(date.text.replace(" 00:00:00", ""))
+	# finds all the divs with the particular class.
+	events = soup.find_all('div', class_ = "date views-row")
+	return events
 
+def transform(articles):
+	for item in articles:
+		# .strip() replaces all of the extra whitespace n the line.
+		name = item.find('span', class_ = 'title').text.strip().replace('\n', '')
+		date = item.find('var', class_ = 'atc_date_start').text.replace(' 00:00:00', '')
+		schoolThing = {
+			'date' : date,
+			'name' : name
+		}
+		main_list.append(schoolThing)
 
-for i in range(0, len(dates)):
-	print(dates[i], " ", names[i])
+def load():
+	df = pd.DataFrame(main_list)
+	df.to_csv('psuDateEvents.csv', index=False)
 
-# print(events[:10])
-# print(names[:10])
-# print(dates[:10])
-
-# def main():
-# 	print("Hello World")
-
-# if __name__ == "__main__":
-# 	main()
+if __name__ == "__main__":
+	url = "https://www.pdx.edu/registration/academic-calendar"
+	data = extract(url)
+	transform(data)
+	load()
+	print('Saved to CSV')
+	# for i in range(0, len(main_list)):
+	# 	print(main_list[i])
